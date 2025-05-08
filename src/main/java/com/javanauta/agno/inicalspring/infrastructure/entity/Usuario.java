67c20d8719faaeaa70743df51587695b2a1f3e79
Agno Santana <153223547+Asantana86@users.noreply.github.com>
2025-05-08 20:57:19 -0300

    Initial commit

---
+++ src/main/java/com/javanauta/agno/inicalspring/infrastructure/entity/Usuario.java
@@ -0,0 +1,46 @@
+package com.javanauta.agno.inicalspring.infrastructure.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+// Cria os metodos get e set com a extensão lombok
+@Setter
+@Getter
+//Cria o contrutor com os atibutos e tambem vazio
+@NoArgsConstructor
+@AllArgsConstructor
+//Demonstra que e uma entidade para o Spring
+@Entity
+//Demosntra que e uma tabela para o Spring
+@Table(name  = "usuario")
+
+public class Usuario {
+
+    //Demosntra de e um coluna e com id primario
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+    //Column demonstr que e uma coluna da tabela
+    @Column(name = "nome", length = 100)
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "senha", length = 10)
+    private String senha;
+
+    //demosntra o relacionamento de um para muitos, no caso que o usuario pode ter mais de um numero de telefone
+    @OneToMany(cascade =  CascadeType.ALL)
+    //define a coluna de junção estrangeira na tabela
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Endereco> enderecos;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Telefone> telefones;
+
+
+}
